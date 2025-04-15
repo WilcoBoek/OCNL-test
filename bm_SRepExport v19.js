94fda61b642f5829b3aca08e7fdc602566638490
WilcoBoek <wilco.boek@rvo.nl>
2025-04-15 11:06:59 +0200

    Update bm_SRepExport v19.js

--- bm_SRepExport v19.js
+++ bm_SRepExport v19.js
@@ -1,4 +1,4 @@
-javascript: (() =>{
+function SRepExoprt() {
 	/* == SearchReport Export v0.19 == */
 	const projID = document.title.substr(document.title.search(": ")+2);
 	const projID1 = (!isNaN(projID)) ? ('EP' + projID) : (projID);
@@ -389,5 +389,4 @@ javascript: (() =>{
 	function sleep(ms){
 		return new Promise(resolve => setTimeout(resolve, ms));
 	}
-
-} )();
+}
